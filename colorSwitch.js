86009f2b67fdcafa1afdc5d37bd19a6c2e0c3347
DmytroPundyk <dimonpund@gmail.com>
2021-10-17 13:01:08 +0300

    colorSwitch

--- colorSwitch.js
+++ colorSwitch.js
@@ -6,3 +6,29 @@ const colors = [
   '#009688',
   '#795548',
 ];
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+};
+const refs = {
+   startBtn: document.querySelector(`button[data-action="start"]`),
+   stopBtn: document.querySelector(`button[data-action="stop"]`),
+   body: document.querySelector(`body`),
+};
+let intervalId = 0;
+
+
+function makeColor() {
+   refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+}
+
+function startChange() {
+   intervalId = setInterval(makeColor, 1000);
+   refs.startBtn.disabled = "true";
+}
+    
+function stopChange() {
+   clearInterval(intervalId);
+   refs.startBtn.disabled = "";
+}
+refs.startBtn.addEventListener("click", startChange);
+refs.stopBtn.addEventListener("click", stopChange);
